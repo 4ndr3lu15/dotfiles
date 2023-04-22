d5dcde335750fc6557e3302305ceb288397d6b0a
cachopa <andreluis@protonmail.com>
2023-04-22 03:15:22 -0300

    wallace

--- backup.sh
+++ backup.sh
@@ -5,4 +5,5 @@ cp -r $HOME/.config/i3 kitty
 cp -r $HOME/.config/i3 nvim 
 cp -r $HOME/.config/i3 picom 
 cp -r $HOME/.config/i3 polybar 
-cp -r $HOME/.config/i3 rofi 
+cp -r $HOME/.config/i3 rofi
+cp -r $HOME/walls walls
