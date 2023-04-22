9588f1a87720fc9a85a95a6104dae08479e4e297
cachopa <andreluis@protonmail.com>
2023-04-22 03:27:23 -0300

    miau

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -r $HOME/.config/i3 i3
-cp -r $HOME/.config/i3 kitty 
-cp -r $HOME/.config/i3 nvim 
-cp -r $HOME/.config/i3 picom 
-cp -r $HOME/.config/i3 polybar 
-cp -r $HOME/.config/i3 rofi
-cp -r $HOME/walls walls
+cp -r $HOME/.config/i3 .
+cp -r $HOME/.config/kitty . 
+cp -r $HOME/.config/nvim . 
+cp -r $HOME/.config/picom . 
+cp -r $HOME/.config/polybar . 
+cp -r $HOME/.config/rofi .
+cp -r $HOME/walls .
