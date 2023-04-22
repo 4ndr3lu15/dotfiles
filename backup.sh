be193bf6089e452181f8cdc9f5b6da685d0c4c4a
cachopa <andreluis@protonmail.com>
2023-04-22 03:07:49 -0300

    the day after

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r $HOME/.config/i3 i3
+cp -r $HOME/.config/i3 kitty 
+cp -r $HOME/.config/i3 nvim 
+cp -r $HOME/.config/i3 picom 
+cp -r $HOME/.config/i3 polybar 
+cp -r $HOME/.config/i3 rofi 
---
+++ populate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -r i3 kitty nvim picom polybar rofi $HOME/.config
